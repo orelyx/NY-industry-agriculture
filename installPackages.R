70bc1afe911be3f9c433bbb0fb987eef09ad0096
Eric Koski <ekoski1@gmail.com>
2021-09-12 15:17:37 -0400

    Removed "library(citr)" from setup.R. We don't need it and it's not available for R 4.1.1 (with which we're now compatible).

--- installPackages.R
+++ installPackages.R
@@ -2,16 +2,17 @@
 #                     GHG inventories. 
 
 install.packages("tidyverse")
+install.packages("tidyselect")
 install.packages("stringi")
 install.packages("readxl")
 install.packages("scales")
 install.packages("fs")
+install.packages("utils")
 install.packages("markdown")
 install.packages("rmarkdown")
 install.packages("kableExtra")
 install.packages("bookdown")
 install.packages("git2r")
-install.packages("citr")
 install.packages("R6")
 install.packages("glue")
 install.packages("latex2exp")
